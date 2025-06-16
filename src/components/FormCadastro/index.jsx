a8436d545177edd5829ef22efc49db26d48fdbec
ThiagoAndrade92 <tihaguinho92@gmail.com>
2025-06-16 14:49:40 -0300

    validação feita

--- src/components/FormCadastro/index.jsx
+++ src/components/FormCadastro/index.jsx
@@ -1,9 +1,61 @@
+import { useState } from 'react';
 import style from './styles.module.css';
 
 export const  FormCadastro= () => {
+    const [ nome, setNome ] = useState('');
+    const [ senha, setSenha ] = useState('');
+    const [ confirmarSenha, setConfirmarSenha ] = useState('');
+
+    const [ erroNome, setErroNome ] = useState('');
+    const [ erroSenha, setErroSenha ] = useState('');
+    const [ erroConfirmarSenha, setErroConfirmarSenha ] = useState('');
+
+    const validarNome = (valor) => {
+        setNome(valor);
+        if (valor.length < 3 || valor.trim() === '') {
+            setErroNome('O campo precisa de no mínimo 3 caracteres.');
+        } else {
+            setErroNome('');
+        }
+    };
+
+    const validarSenha = (valor) => {
+        setSenha(valor);
+        if(valor.length < 6 || valor.trim() === '') {
+            setErroSenha('A senha deve ter pelo menos 6 caracteres.');
+        } else {
+            setErroSenha('');
+        }
+    };
+
+    const validarConfirmarSenha = (valor) => {
+        setConfirmarSenha(valor);
+        if(valor !== senha || valor.trim() === '') {
+            setErroConfirmarSenha('As senhas não coincidem.')
+        } else {
+            setErroConfirmarSenha('');
+        }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (erroNome || erroSenha || erroConfirmarSenha) {
+            alert('Corrija os erros antes de enviar.');
+            return;
+        }
+
+        setNome('');
+        setSenha('');
+        setConfirmarSenha('');
+
+        console.log(nome, senha, confirmarSenha)
+    };
+
+
   return (
     <div className={style.form}>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2 className={style.titulo}>Cadastre-se</h2>
             <div className={style.inputs}>
                 <label className={style.label}>
@@ -12,7 +64,10 @@ export const  FormCadastro= () => {
                     type="text"
                     id="nome"
                     placeholder='Digite seu nome'
+                    value={nome}
+                    onChange={e => validarNome(e.target.value)}
                     />
+                    {erroNome && <span className={style.erro}>{erroNome}</span>}
                 </label>
 
                 <label className={style.label}>
@@ -21,16 +76,22 @@ export const  FormCadastro= () => {
                     type="password"
                     id="senha"
                     placeholder='Digite sua senha'
+                    value={senha}
+                    onChange={e => validarSenha(e.target.value)}
                     />
+                    {erroSenha && <p className={style.erro}>{erroSenha}</p>}
                 </label>
 
                 <label className={style.label}>
-                    Corfirmar Senha:
+                    Confirmar Senha:
                     <input
                     type="password"
-                    id="corfirmar_senha"
+                    id="confirmar_senha"
                     placeholder='Confirme sua senha'
+                    value={confirmarSenha}
+                    onChange={e => validarConfirmarSenha(e.target.value)}
                     />
+                    {erroConfirmarSenha && <p className={style.erro}>{erroConfirmarSenha}</p>}
                 </label>
             </div>
             <button type="submit" className={style.btn}>Enviar</button>
